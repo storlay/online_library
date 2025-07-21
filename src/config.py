5874f57ea443faf18bb301c94db05bbfaa1bd868
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2025-07-21 09:03:24 -0300

    Add base app settings

---
+++ src/config.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+from typing import Literal
+
+from pydantic import BaseModel
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent.parent
+
+
+class DatabaseSettings(BaseModel):
+    NAME: str = os.getenv(
+        "POSTGRES_DB",
+        "booking",
+    )
+    USER: str = os.getenv(
+        "POSTGRES_USER",
+        "admin",
+    )
+    PASS: str = os.getenv(
+        "POSTGRES_PASSWORD",
+        "admin",
+    )
+    HOST: str = os.getenv(
+        "DB_HOST",
+        "localhost",
+    )
+    PORT: int = os.getenv(
+        "DB_PORT",
+        5433,
+    )
+    URL: PostgresDsn = f"postgresql+asyncpg://{USER}:{PASS}@{HOST}:{PORT}/{NAME}"
+
+
+class AppSettings(BaseModel):
+    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"] = os.getenv("APP_MODE")
+
+
+class Settings(BaseSettings):
+    db: DatabaseSettings = DatabaseSettings()
+    app: AppSettings = AppSettings()
+
+
+settings = Settings()
