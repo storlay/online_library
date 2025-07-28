94cff4f425ced67021666ade4baac93713b48438
Dmitrii Ivanov <dmitriy.site1@gmail.com>
2025-07-28 13:15:01 -0300

    Refactoring auth service

--- src/services/auth.py
+++ src/services/auth.py
@@ -1,12 +1,17 @@
-import bcrypt
+from datetime import UTC
+from datetime import datetime
+from datetime import timedelta
 
+import bcrypt
+import jwt
+
+from src.config import settings
 from src.exceptions.repository.base import CannotAddObjectRepoException
 from src.exceptions.service.user import UserAlreadyExistsServiceException
 from src.schemas.auth import JWTInfoSchema
 from src.schemas.user import UserAuthSchema
 from src.schemas.user import UserSchema
 from src.services.base import BaseService
-from src.services.jwt import JWTService
 
 
 class AuthService(BaseService):
@@ -28,8 +33,8 @@ class AuthService(BaseService):
         self,
         user_id: int,
     ) -> JWTInfoSchema:
-        access_token = JWTService().create_access_token_for_user(user_id)
-        refresh_token = JWTService().create_refresh_token_for_user(user_id)
+        access_token = self.create_access_token_for_user(user_id)
+        refresh_token = self.create_refresh_token_for_user(user_id)
         return JWTInfoSchema(
             access=access_token,
             refresh=refresh_token,
@@ -49,7 +54,7 @@ class AuthService(BaseService):
         self,
         user_id: int,
     ) -> JWTInfoSchema:
-        access_token = JWTService().create_access_token_for_user(user_id)
+        access_token = self.create_access_token_for_user(user_id)
         return JWTInfoSchema(
             access=access_token,
         )
@@ -62,4 +67,63 @@ class AuthService(BaseService):
         return bcrypt.checkpw(
             password.encode("utf-8"),
             hashed_password,
+        )
+
+    def create_access_token_for_user(
+        self,
+        user_id: int,
+    ):
+        base_payload = {
+            "sub": str(user_id),
+            "owner": settings.jwt.OWNER,
+            settings.jwt.TYPE_FIELD: settings.jwt.ACCESS_TOKEN_TYPE,
+        }
+        return self._encode(
+            base_payload,
+            settings.jwt.ACCESS_TOKEN_EXPIRE_TIMEDELTA,
+        )
+
+    def create_refresh_token_for_user(
+        self,
+        user_id: int,
+    ) -> str:
+        base_payload = {
+            "sub": str(user_id),
+            settings.jwt.TYPE_FIELD: settings.jwt.REFRESH_TOKEN_TYPE,
+        }
+        return self._encode(
+            base_payload,
+            settings.jwt.REFRESH_TOKEN_EXPIRE_TIMEDELTA,
+        )
+
+    def _encode(
+        self,
+        payload: dict,
+        expire_timedelta: timedelta,
+        private_key: str = settings.jwt.PRIVATE_KEY_PATH.read_text(),
+        algorithm: str = settings.jwt.ALGORITHM,
+    ) -> str:
+        to_encode = payload.copy()
+        now = datetime.now(UTC)
+        expire = now + expire_timedelta
+        to_encode.update(
+            exp=expire,
+            iat=now,
+        )
+        return jwt.encode(
+            to_encode,
+            private_key,
+            algorithm=algorithm,
+        )
+
+    def decode(
+        self,
+        token: str | bytes,
+        public_key: str = settings.jwt.PUBLIC_KEY_PATH.read_text(),
+        algorithm: str = settings.jwt.ALGORITHM,
+    ) -> dict:
+        return jwt.decode(
+            token,
+            public_key,
+            algorithms=[algorithm],
         )
